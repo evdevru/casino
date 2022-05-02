8a8454d697bb02e5ffcbc6177d9d2fedebe21dad
Evgeniy <76659477+evdevru@users.noreply.github.com>
2022-05-02 18:13:05 +0300

    update handler

--- plugins/axios-handler.client.js
+++ plugins/axios-handler.client.js
@@ -1,5 +1,6 @@
 export default function ({ $axios, $notify }) {
   $axios.onResponse((response) => {
+    if (response && response.data) {
     const { status, message } = response.data
     if (status && message)
       $notify(
@@ -10,11 +11,12 @@ export default function ({ $axios, $notify }) {
         },
         4000
       )
+    }
   })
   $axios.onError((error) => {
     let message = null
     // validation errors in error.response.data
-    if (error.response.data.message) {
+    if (error.response?.data?.message) {
       $notify(
         {
           text: error.response.data.message,
@@ -25,7 +27,7 @@ export default function ({ $axios, $notify }) {
       )
     }
 
-    if (!error.response.data.message && Array.isArray(error.response.data)) {
+    if (!error.response?.data?.message && Array.isArray(error.response.data)) {
       error.response.data.forEach((err) => {
         $notify(
           {
@@ -38,7 +40,7 @@ export default function ({ $axios, $notify }) {
       })
     }
 
-    const code = error.response.status
+    const code = error.response?.status
 
     switch (code) {
       case 401:
